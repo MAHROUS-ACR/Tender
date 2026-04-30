c1c0df048d54ce33a1573e401aac63a5beec4f44
MAHROUS-ACR <mahrous.ref@gmail.com>
2026-04-30 20:32:41 +0300

    Update Firebase configuration to use serviceAccount.json

--- firebase_config.py
+++ firebase_config.py
@@ -1,13 +1,7 @@
-import os
-import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred_json = os.getenv("FIREBASE_CREDENTIALS")
-
-cred_dict = json.loads(cred_json)
-
-cred = credentials.Certificate(cred_dict)
+cred = credentials.Certificate("serviceAccount.json")
 
 if not firebase_admin._apps:
     firebase_admin.initialize_app(cred)
