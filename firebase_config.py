a8cb1e21870a5106a6890d06e654d8429aad0766
MAHROUS-ACR <mahrous.ref@gmail.com>
2026-04-30 20:52:21 +0300

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -3,7 +3,10 @@ import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred_json = os.getenv("FIREBASE_CREDENTIALS")
+cred_json = os.getenv("FIREBASE_KEY")  # 👈 نفس اسم السيكرت عندك
+
+if not cred_json:
+    raise Exception("FIREBASE_KEY is missing from environment variables")
 
 cred_dict = json.loads(cred_json)
 
